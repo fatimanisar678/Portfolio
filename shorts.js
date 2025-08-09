4d9be0799a9aead48f903a501e65a9a3cf58792a
Fatima Nisar <fatimanisar477@gmail.com>
2025-08-09 16:43:49 +0500

    Auto-scroll and change of css

--- shorts.js
+++ shorts.js
@@ -1,12 +1,55 @@
+const shortsScrollWrapper = document.querySelector('.shorts-section .video-scroll-wrapper');
 
-  document.querySelectorAll('video').forEach(video => {
-    video.addEventListener('mouseenter', () => {
-      video.muted = false;
-      video.play();
-    });
+let shortsAutoScrollInterval;
+let shortsIsUserScrolling = false;
 
-    video.addEventListener('mouseleave', () => {
-      video.pause();
-    });
-  });
+function startShortsAutoScroll() {
+  shortsAutoScrollInterval = setInterval(() => {
+    if (!shortsIsUserScrolling) {
+      shortsScrollWrapper.scrollLeft += 1;
 
+      if (shortsScrollWrapper.scrollLeft >= shortsScrollWrapper.scrollWidth - shortsScrollWrapper.clientWidth) {
+        shortsScrollWrapper.scrollLeft = 0;
+      }
+    }
+  }, 20);
+}
+
+function stopShortsAutoScroll() {
+  clearInterval(shortsAutoScrollInterval);
+}
+
+// User interaction handlers
+shortsScrollWrapper.addEventListener('mousedown', () => {
+  shortsIsUserScrolling = true;
+  stopShortsAutoScroll();
+});
+
+shortsScrollWrapper.addEventListener('mouseup', () => {
+  shortsIsUserScrolling = false;
+  startShortsAutoScroll();
+});
+
+shortsScrollWrapper.addEventListener('touchstart', () => {
+  shortsIsUserScrolling = true;
+  stopShortsAutoScroll();
+});
+
+shortsScrollWrapper.addEventListener('touchend', () => {
+  shortsIsUserScrolling = false;
+  startShortsAutoScroll();
+});
+
+// Pause on hover
+shortsScrollWrapper.addEventListener('mouseenter', () => {
+  shortsIsUserScrolling = true;
+  stopShortsAutoScroll();
+});
+
+shortsScrollWrapper.addEventListener('mouseleave', () => {
+  shortsIsUserScrolling = false;
+  startShortsAutoScroll();
+});
+
+// Start auto-scroll initially
+startShortsAutoScroll();
